1aa7afe98e471acf98c8af5b3469461b0d84a70e
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-01-02 18:28:20 +0700

    update index.js
    change remove to delete text in delincart function

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ function deinitems(action, index) {
             if(cart[index].count <= 0) {
                 Swal.fire({
                     icon: 'warning',
-                    title: 'Are you sure to remove?',
+                    title: 'Are you sure to delete?',
                     showConfirmButton: true,
                     showCancelButton: true,
                     confirmButtonText: 'Delete',
